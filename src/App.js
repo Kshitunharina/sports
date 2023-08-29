dac40e1ce310a8917e0f07680cf1828ee6b0cb33
Kshitunharina <kshitunharina@gmail.com>
2023-08-29 16:35:11 +0530

    sports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Sports from './Sports'
+import Navbar from './Nav'
+import Header from './Header'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //  <Home></Home>
+  <BrowserRouter>
+  <Navbar></Navbar>
+  <Routes>
+    {/* <Route path="/" element={<Home></Home>}> </Route> */}
+    
+    <Route path='/header' element={<Header></Header>}></Route>
+    <Route path='/sports' element={<Sports></Sports>}></Route>
+  </Routes>
+  </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
+
+
+// import React from 'react'
+// import { Container,Row,Col } from 'reactstrap'
+// import Example from './Example'
+
+// function App() {
+//   return (
+//    <Container>
+//     <Row>
+//     <Example></Example>
+//     </Row>
+//    </Container>
+//   )
+// }
+
+// export default App
