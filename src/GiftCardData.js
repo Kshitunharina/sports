2d593070184bd86c38b57ee8afd1aa551b7de1f9
Kshitunharina <kshitunharina@gmail.com>
2023-09-06 12:28:23 +0530

    modify

---
+++ src/GiftCardData.js
@@ -0,0 +1,112 @@
+export const giftCardData = [
+  {
+    id: 1,
+    img: "https://in.bmscdn.com/gv/gift_my_show_02402022094010_480x295.jpg",
+    catagory: "generic",
+  },
+  {
+    id: 2,
+    img: "https://in.bmscdn.com/gv/gift_my_show_02282022102856_480x295.jpg",
+    catagory: "generic",
+  },
+  {
+    id: 3,
+    img: "https://in.bmscdn.com/gv/gift_my_show_02302022103011_480x295.jpg",
+    catagory: "generic",
+  },
+  {
+    id: 4,
+    img: "https://in.bmscdn.com/gv/gift_my_show_02302022123034_480x295.jpg",
+    catagory: "generic",
+  },
+  {
+    id: 5,
+    img: "https://in.bmscdn.com/gv/gift_my_show_02412022124146_480x295.jpg",
+    catagory: "generic",
+  },
+  {
+    id: 6,
+    img: "https://in.bmscdn.com/gv/gift_my_show_02432022124329_480x295.jpg",
+    catagory: "generic",
+  },
+  {
+    id: 7,
+    img: "https://in.bmscdn.com/gv/gift_my_show_02442022124437_480x295.jpg",
+    catagory: "generic",
+  },
+  {
+    id: 8,
+    img: "https://in.bmscdn.com/gv/gift_my_show_02452022124540_480x295.jpg",
+    catagory: "generic",
+  },
+  {
+    id: 9,
+    img: "https://in.bmscdn.com/gv/gift_my_show_18312019023126_480x295.jpg",
+    catagory: "occasion",
+  },
+  {
+    id: 10,
+    img: "https://in.bmscdn.com/gv/gift_my_show_18432019024353_480x295.jpg",
+    catagory: "occasion",
+  },
+  {
+    id: 11,
+    img: "https://in.bmscdn.com/gv/gift_my_show_18462019024648_480x295.jpg",
+    catagory: "occasion",
+  },
+  {
+    id: 12,
+    img: "https://in.bmscdn.com/gv/gift_my_show_18482019024856_480x295.jpg",
+    catagory: "occasion",
+  },
+  {
+    id: 13,
+    img: "https://in.bmscdn.com/gv/gift_my_show_18512019025105_480x295.jpg",
+    catagory: "occasion",
+  },
+  {
+    id: 14,
+    img: "https://in.bmscdn.com/gv/gift_my_show_18572019025701_480x295.jpg",
+    catagory: "occasion",
+  },
+  {
+    id: 15,
+    img: "https://in.bmscdn.com/gv/gift_my_show_30162019031622_480x295.jpg",
+    catagory: "occasion",
+  },
+  {
+    id: 16,
+    img: "https://in.bmscdn.com/vouchagram/combo/bms_and_the_beer_cafe_combo_20302018053047.jpg",
+    catagory: "combos",
+  },
+  {
+    id: 17,
+    img: "https://in.bmscdn.com/vouchagram/combo/bms_and_tgi_fridays_combo_20042018040430.jpg",
+    catagory: "combos",
+  },
+  {
+    id: 18,
+    img: "https://in.bmscdn.com/vouchagram/combo/bms_and_speciality_restaurants_combo_20342018043406.jpg",
+    catagory: "combos",
+  },
+  {
+    id: 19,
+    img: "https://in.bmscdn.com/vouchagram/combo/bms_and_vero_moda_combo_08352018123514.jpg",
+    catagory: "combos",
+  },
+  {
+    id: 20,
+    img: "https://in.bmscdn.com/vouchagram/combo/bms_and_levis_combo_07132022031353.jpg",
+    catagory: "combos",
+  },
+  {
+    id: 21,
+    img: "https://in.bmscdn.com/vouchagram/combo/united_colors_of_benetton_and_bms_combo_08432018124326.jpg",
+    catagory: "combos",
+  },
+  {
+    id: 22,
+    img: "https://in.bmscdn.com/vouchagram/combo/hidesign_and_bms_combo_08552018125551.jpg",
+    catagory: "combos",
+  },
+];
--- src/Giftcard.js
+++ src/Giftcard.js
@@ -1,164 +1,31 @@
-import React from "react";
-import "./Giftcards.css";
+import React, { useState } from "react";
 import "./Giftcard.css";
-import Giftcards from "./Giftcards";
-import Giftcard2 from "./Giftcard2.js";
-import Giftcard3 from "./Giftcard3.js";
-import Giftcard4 from "./Giftcard4.js";
-import Giftcard5 from "./Giftcard5.js";
-import Giftcard6 from "./Giftcard6.js";
-import Giftcard7 from "./Giftcard7.js";
-import Giftcard8 from "./Giftcard8.js";
-import Giftcard9 from "./Giftcard9.js";
-import Giftcard10 from "./Giftcard10.js";
-const card1 = [
-  {
-    gift1: "https://in.bmscdn.com/gv/gift_my_show_24102023031014_480x295.jpg",
-  },
-  {
-    gift1: "https://in.bmscdn.com/gv/gift_my_show_24052023030554_480x295.jpg",
-  },
-  {
-    gift1: "https://in.bmscdn.com/gv/gift_my_show_02402022094010_480x295.jpg",
-  },
-  {
-    gift1: "https://in.bmscdn.com/gv/gift_my_show_02282022102856_480x295.jpg",
-  },
-];
-const card2 = [
-  {
-    gift2: "https://in.bmscdn.com/gv/gift_my_show_02302022103011_480x295.jpg",
-  },
-  {
-    gift2: "https://in.bmscdn.com/gv/gift_my_show_02302022123034_480x295.jpg",
-  },
-  {
-    gift2: "https://in.bmscdn.com/gv/gift_my_show_02412022124146_480x295.jpg",
-  },
-  {
-    gift2: "https://in.bmscdn.com/gv/gift_my_show_02432022124329_480x295.jpg",
-  },
-];
-const card3 = [
-  {
-    gift3: "https://in.bmscdn.com/gv/gift_my_show_02442022124437_480x295.jpg",
-  },
-  {
-    gift3: "https://in.bmscdn.com/gv/gift_my_show_02452022124540_480x295.jpg",
-  },
-  {
-    gift3: "https://in.bmscdn.com/gv/gift_my_show_02122022101208_480x295.jpg",
-  },
-  {
-    gift3: "https://in.bmscdn.com/gv/gift_my_show_25412019034153_480x295.jpg",
-  },
-];
-const card4 = [
-  {
-    gift4: "https://in.bmscdn.com/gv/gift_my_show_27542019095448_480x295.jpg",
-  },
-  {
-    gift4: "https://in.bmscdn.com/gv/gift_my_show_27572019095730_480x295.jpg",
-  },
-  {
-    gift4: "https://in.bmscdn.com/gv/gift_my_show_18532019025324_480x295.jpg",
-  },
-  {
-    gift4: "https://in.bmscdn.com/gv/gift_my_show_18552019025553_480x295.jpg",
-  },
-];
-const card5 = [
-  {
-    gift5: "https://in.bmscdn.com/gv/gift_my_show_18082019040833_480x295.jpg",
-  },
-  {
-    gift5: "https://in.bmscdn.com/gv/gift_my_show_18312019023126_480x295.jpg",
-  },
-  {
-    gift5: "https://in.bmscdn.com/gv/gift_my_show_18432019024353_480x295.jpg",
-  },
-  {
-    gift5: "https://in.bmscdn.com/gv/gift_my_show_18462019024648_480x295.jpg",
-  },
-];
-const card6 = [
-  {
-    gift6: "https://in.bmscdn.com/gv/gift_my_show_18482019024856_480x295.jpg",
-  },
-  {
-    gift6: "https://in.bmscdn.com/gv/gift_my_show_18512019025105_480x295.jpg",
-  },
-  {
-    gift6: "https://in.bmscdn.com/gv/gift_my_show_18572019025701_480x295.jpg",
-  },
-  {
-    gift6: "https://in.bmscdn.com/gv/gift_my_show_05542019115440_480x295.jpg",
-  },
-];
-const card7 = [
-  {
-    gift7: "https://in.bmscdn.com/gv/gift_my_show_30332019123339_480x295.jpg",
-  },
-  {
-    gift7: "https://in.bmscdn.com/gv/gift_my_show_30222019022237_480x295.jpg",
-  },
-  {
-    gift7: "https://in.bmscdn.com/gv/gift_my_show_30192019031956_480x295.jpg",
-  },
-  {
-    gift7: "https://in.bmscdn.com/gv/gift_my_show_30482019034817_480x295.jpg",
-  },
-];
-const card8 = [
-  {
-    gift8: "https://in.bmscdn.com/gv/gift_my_show_05572019115712_480x295.jpg",
-  },
-  {
-    gift8: "https://in.bmscdn.com/gv/gift_my_show_16092019040918_480x295.jpg",
-  },
-  {
-    gift8: "https://in.bmscdn.com/gv/gift_my_show_06532019115311_480x295.jpg",
-  },
-  {
-    gift8: "https://in.bmscdn.com/gv/gift_my_show_22202019042033_480x295.jpg",
-  },
-];
-const card9 = [
-  {
-    gift9: "https://in.bmscdn.com/gv/gift_my_show_30162019031622_480x295.jpg",
-  },
-  {
-    gift9: "https://in.bmscdn.com/gv/gift_my_show_06572019115735_480x295.jpg",
-  },
-  {
-    gift9:
-      "https://in-aps.bmscdn.com/gv/gift_my_show_04402020084029_480x295.jpg",
-  },
-  {
-    gift9:
-      "https://in.bmscdn.com/vouchagram/combo/bms_and_the_beer_cafe_combo_20302018053047.jpg",
-  },
-];
-const card10 = [
-  {
-    gift10:
-      "https://in.bmscdn.com/vouchagram/combo/bms_and_tgi_fridays_combo_20042018040430.jpg",
-  },
-  {
-    gift10:
-      "https://in.bmscdn.com/vouchagram/combo/bms_and_speciality_restaurants_combo_20342018043406.jpg",
-  },
-  {
-    gift10:
-      "https://in.bmscdn.com/vouchagram/combo/bms_and_vero_moda_combo_08352018123514.jpg",
-  },
-  {
-    gift10:
-      "https://in.bmscdn.com/vouchagram/combo/bms_and_levis_combo_07132022031353.jpg",
-  },
-];
+import { giftCardData } from "./GiftCardData";
 
 function Giftcard() {
+  const [activeclass, setactive] = useState([true, false, false, false, false]);
+
+  const generic = filterFunction("generic");
+  const occasion = filterFunction("occasion");
+  const combos = filterFunction("combos");
+
+  function multipleStates(index) {
+    const newActiveBtn = activeclass.map((ele, i) => {
+      if (index === i) {
+        return !ele;
+      }
+      return false;
+    });
+    setactive(newActiveBtn);
+  }
+
+  function filterFunction(catagory) {
+    const arr = giftCardData.filter((el) => {
+      return el.catagory === catagory;
+    });
+    return arr;
+  }
+
   return (
     <div className="GiftCards" style={{ border: "2px solid black" }}>
       <div className="f-picture">
@@ -175,66 +42,103 @@ function Giftcard() {
             <a href="">Physical Giftcards</a>
           </button>
         </div>
-        <p style={{ textAlign: "center",width:"100vw" }}>
+        <p style={{ textAlign: "center", width: "100vw" }}>
           Pick a card from one of our themes
         </p>
-       <div className="filtergift">
-       <button><a href="">All</a></button>
-        <button><a href="">Generic</a></button>
-        <button><a href="">Occasion</a></button>
-        <button><a href="">Festival</a></button>
-        <button><a href="" style={{color:"white"}}>Combos</a></button>
-       </div>
+        <div className="filtergift">
+          <button
+            className={
+              activeclass[0] ? `giftcardactive giftcardbtns` : `giftcardbtns`
+            }
+            onClick={() => {
+              multipleStates(0);
+            }}
+          >
+            All
+          </button>
+          <button
+            className={
+              activeclass[1] ? `giftcardactive giftcardbtns` : `giftcardbtns`
+            }
+            onClick={() => {
+              multipleStates(1);
+            }}
+          >
+            Generic
+          </button>
+          <button
+            className={
+              activeclass[2] ? `giftcardactive giftcardbtns` : `giftcardbtns`
+            }
+            onClick={() => {
+              multipleStates(2);
+            }}
+          >
+            Occasion
+          </button>
+          <button
+            className={
+              activeclass[3] ? `giftcardactive giftcardbtns` : `giftcardbtns`
+            }
+            onClick={() => {
+              multipleStates(3);
+            }}
+          >
+            Festival
+          </button>
+          <button
+            className={
+              activeclass[4] ? `giftcardactive giftcardbtns` : `giftcardbtns`
+            }
+            onClick={() => {
+              multipleStates(4);
+            }}
+          >
+            Combos
+          </button>
+        </div>
       </div>
-      <div className="f-row">
-        {card1.map((giftcd1) => {
-          return <Giftcards gtcard={giftcd1.gift1}></Giftcards>;
+<div className="toggle--btn" style={{ display:"grid",gridTemplateColumns:"1fr 1fr 1fr 1fr"}}>
+      {activeclass[0] &&
+        giftCardData.map((el) => {
+          return (
+            <div className="gift__card" key={el.id} >
+              <img src={el.img} />
+            </div>
+          );
+        })
+      }
+
+      {activeclass[1] &&
+        generic.map((el) => {
+          return (
+            <div className="gift__card" key={el.id}>
+              <img src={el.img} />
+            </div>
+          );
+        })
+        
+        
+      }
+
+      {activeclass[2] &&
+        occasion.map((el) => {
+          return (
+            <div className="gift__card" key={el.id}>
+              <img src={el.img} />
+            </div>
+          );
         })}
-      </div>
-      <div className="s-row">
-        {card2.map((giftcd2) => {
-          return <Giftcard2 gtcard2={giftcd2.gift2}></Giftcard2>;
-        })}
-      </div>
-      <div className="t-row">
-        {card3.map((giftcd3) => {
-          return <Giftcard3 gtcard3={giftcd3.gift3}></Giftcard3>;
-        })}
-      </div>
-      <div className="fr-row">
-        {card4.map((giftcd4) => {
-          return <Giftcard4 gtcard4={giftcd4.gift4}></Giftcard4>;
-        })}
-      </div>
-      <div className="ft-row">
-        {card5.map((giftcd5) => {
-          return <Giftcard5 gtcard5={giftcd5.gift5}></Giftcard5>;
-        })}
-      </div>
-      <div className="si-row">
-        {card6.map((giftcd6) => {
-          return <Giftcard6 gtcard6={giftcd6.gift6}></Giftcard6>;
-        })}
-      </div>
-      <div className="svn-row">
-        {card7.map((giftcd7) => {
-          return <Giftcard7 gtcard7={giftcd7.gift7}></Giftcard7>;
-        })}
-      </div>
-      <div className="e-row">
-        {card8.map((giftcd8) => {
-          return <Giftcard8 gtcard8={giftcd8.gift8}></Giftcard8>;
-        })}
-      </div>
-      <div className="n-row">
-        {card9.map((giftcd9) => {
-          return <Giftcard9 gtcard9={giftcd9.gift9}></Giftcard9>;
-        })}
-      </div>
-      <div className="tn-row">
-        {card10.map((giftcd10) => {
-          return <Giftcard10 gtcard10={giftcd10.gift10}></Giftcard10>;
-        })}
+
+      {activeclass[4] &&
+        combos.map((el) => {
+          return (
+            <div className="gift__card" key={el.id}>
+              <img src={el.img} />
+            </div>
+          );
+        })
+      }
       </div>
     </div>
   );
